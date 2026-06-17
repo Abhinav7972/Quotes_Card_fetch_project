cdf107c3d24a6f21dcee760076e4ae73ea81224f
abhinav soni <abhinavsoni316@gmail.com>
2026-06-17 14:52:31 +0530

    added promise chain functionality

--- script.js
+++ script.js
@@ -4,28 +4,52 @@ const card_container = document.querySelector(".card-container");
 
 
 function getID(id)
-{
-    fetch(`https://dummyjson.com/quotes/${id}`)
-        .then(res => res.json())
-        .then((res) => {
-            displayquote(res);
-        })
+{   
+
+        fetch(`https://dummyjson.com/quotes/${id}`)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('id not found')
+                }
+                return res.json();
+            })
+            .then((res) => {
+                displayquote(res, 'beforeend');
+                console.log('next', res);
+                return fetch(`https://dummyjson.com/quotes/${id-1}`)
+            })
+            .catch(err => console.error(err))
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('previous id not found')
+                }
+                return res.json();
+            })
+    
+            .then((res) => {
+                displayquote(res, 'beforeend');
+                console.log('previous', res);
+                return fetch(`https://dummyjson.com/quotes/${id - 1}`)
+            })
+            .catch(err => console.error(err))
+    
 }
 
 
-function displayquote(data)
+function displayquote(data,pos)
 {
     const card = `<div class="card">
             <h3 id="quote">${data.quote}</h3>
              <p id="author">${data.author}</p>
         </div>`
     
-    card_container.insertAdjacentHTML("beforeend", card);
+    card_container.insertAdjacentHTML(pos, card);
 }
 
 
 
-for (let i = 1; i <= 12; i++)
-{
+
+
+for(let i = 4; i <= 10; i += 2) {
     getID(i);
 }
