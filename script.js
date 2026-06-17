9d4c8875a2c4109fb1501a15cf7de346590eaed1
abhinav soni <abhinavsoni316@gmail.com>
2026-06-17 11:51:08 +0530

    added logic to render cards using fetch api

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const card_container = document.querySelector(".card-container");
+
+
+
+function getID(id)
+{
+    fetch(`https://dummyjson.com/quotes/${id}`)
+        .then(res => res.json())
+        .then((res) => {
+            displayquote(res);
+        })
+}
+
+
+function displayquote(data)
+{
+    const card = `<div class="card">
+            <h3 id="quote">${data.quote}</h3>
+             <p id="author">${data.author}</p>
+        </div>`
+    
+    card_container.insertAdjacentHTML("beforeend", card);
+}
+
+
+
+for (let i = 1; i <= 6; i++)
+{
+    getID(i);
+}
